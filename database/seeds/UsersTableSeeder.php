73a01c4d824598cab19ebf8527f29f84b611aab7
Jjx51 <jjx51@outlook.com>
2018-01-06 19:33:58 -0600

    Creado el Factorie para la tabla de usuarios

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -26,5 +26,7 @@ class UsersTableSeeder extends Seeder
             'email'=>'kroer17@gmail.com',
             'password'=>bcrypt('admin')
         ]);
+
+        factory(App\User::class,2)->create();
     }
 }
