24c46cb2f6038bf8bb97baa7f5e5a453617dd866
Jjx51 <jjx51@outlook.com>
2018-01-06 19:28:40 -0600

    Creado el Seeder para usuario con cuenta admin y user

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class)->create([
+        	'name'=>'Jorge Nuñez',
+            'username'=>'jjx51',
+        	'rol'=>'admin',
+        	'email'=>'jjx51@outlook.com',
+        	'password'=>bcrypt('admin')
+        ]);
+
+        factory(App\User::class)->create([
+            'name'=>'Miguel Jimenez',
+            'username'=>'kroer17',
+            'rol'=>'user',
+            'email'=>'kroer17@gmail.com',
+            'password'=>bcrypt('admin')
+        ]);
+    }
+}
