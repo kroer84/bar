e56312b50b8e747303fa4f4cc80cfc89a8310185
kroer84 <kroer17@hotmail.com>
2018-01-20 18:12:26 -0600

    StatusCountProduct(Modelo, migracion, seeder)

---
+++ app/StatusCountProduct.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StatusCountProduct extends Model
+{
+    /* 
+    public function CountProduct()
+    {
+        return $this->hasMany('App\Count_Product','state_count__products_id','id');
+    } 
+    */
+
+}
---
+++ database/seeds/StatusCountProductSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\StatusCountProduct;
+
+class StatusCountProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        StatusCountProduct::truncate();
+        StatusCountProduct::create([
+            'NombreEstado' => 'Por Confirmar'
+        ]);
+
+        StatusCountProduct::create([
+            'NombreEstado' => 'confirmado'
+        ]);
+
+    }
+}
