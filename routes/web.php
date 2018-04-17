87175ddc49de99286bb7154ba6f8843ddda7c671
kroer84 <kroer17@hotmail.com>
2018-04-17 14:59:38 -0500

    Simplificacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,21 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+/*----------------------------------Login----------------------------------------- */
+Route::view('/','welcome');
+/*---------------------------------------------------------------------------------- */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*----------------------------------Manejo de Cuentas------------------------------- */
+Route::view('/inicio','home')->name('inicio');
+/*-----------------------------------------------------------------------------------*/
 
+/*----------------------------Manejo de usuarios------------------------------------*/
 Auth::routes();
-
-Route::get('/inicio', 'HomeController@index')->name('inicio');
-
-/*Esta ruta sirve unicamente como prueba */
-Route::get('/prueba',function(){
-	return view('products.create');
-});
-
 Route::get('/usuarios', 'Auth\RegisterController@index')->name('register.index');
-
-/*Falta checar que nombre va a tener esta ruta porque por defecto ocupa la funcion create pero lo ocupa para crear un nuevo usuario y no para mostrar el formulario*/
 Route::get('/usuarios/crear', 'Auth\RegisterController@index')->name('register.create');
+/*----------------------------------------------------------------------------------*/
 
+
+/*-------------------------------Manejo de PDF--------------------------------------*/
 Route::get('/ticket', 'PdfController@index')->name('generate-pdf');
+/*---------------------------------------------------------------------------------- */
