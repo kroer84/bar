55b3547aae21903e8db734037b54ffeb376141d2
kroer84 <kroer17@hotmail.com>
2018-04-20 00:23:17 -0500

    modificacionde seeder

--- database/factories/CountProductFactory.php
+++ database/factories/CountProductFactory.php
@@ -4,8 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\CountProduct::class, function (Faker $faker) {
     return [
-        'counts_id' =>$faker->numberBetween(1, 4),
-        'products_id'=>$faker->numberBetween(1, 8),
+        'counts_id' =>$faker->numberBetween(1, 100),
+        'products_id'=>$faker->numberBetween(1, 23),
         'cantidad'=>$faker->numberBetween(1, 10),
         'status_count_products_id'=>$faker->numberBetween(1, 2),
     ];
